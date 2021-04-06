28f321c20ac5c85eaa8fee3dc52258f87f3f0057
katemejaski <katmackjj@gmail.com>
2021-04-06 16:51:06 -0400

    First Commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBhIyqA7MkwoC9odeczR4yl5qTPhTUpxsg",
+    authDomain: "react-firechat2.firebaseapp.com",
+    projectId: "react-firechat2",
+    storageBucket: "react-firechat2.appspot.com",
+    messagingSenderId: "620546647838",
+    appId: "1:620546647838:web:b3c1fe58a472cfd72ccb0f"
+  };
+  export default firebaseConfig;
